697f5a89fd65b0cdd9358be9757339dd3797cb7f
Liquidsoul <liquidsoul@liquidsoul.fr>
2017-04-22 09:47:53 +0200

    Remove duplication in template but at the cost of generated code readability

--- Sources/AutoJSONDeserializable/Extensions/AutoJSONDeserializable.generated.swift
+++ Sources/AutoJSONDeserializable/Extensions/AutoJSONDeserializable.generated.swift
@@ -8,13 +8,17 @@
 // MARK: - JSONDeserializableProperty AutoJSONDeserializable
 extension JSONDeserializableProperty: JSONDeserializable {
     internal init?(JSONObject: [String: Any]) {
-        guard let entity = (JSONObject["entity"] as? [String: Any]).flatMap(Entity.init(JSONObject:)) else { return nil }
+        guard
+        let entity = (JSONObject["entity"] as? [String: Any]).flatMap(Entity.init(JSONObject:))
+        else { return nil }
         self.entity = entity
         let optionalEntity = (JSONObject["optionalEntity"] as? [String: Any]).flatMap(Entity.init(JSONObject:))
         self.optionalEntity = optionalEntity
         let nilEntity = (JSONObject["nilEntity"] as? [String: Any]).flatMap(Entity.init(JSONObject:))
         self.nilEntity = nilEntity
-        guard let annotatedEntity = (JSONObject["annotated_entity"] as? [String: Any]).flatMap(Entity.init(JSONObject:)) else { return nil }
+        guard
+        let annotatedEntity = (JSONObject["annotated_entity"] as? [String: Any]).flatMap(Entity.init(JSONObject:))
+        else { return nil }
         self.annotatedEntity = annotatedEntity
         let optionalAnnotatedEntity = (JSONObject["optional_annotated_entity"] as? [String: Any]).flatMap(Entity.init(JSONObject:))
         self.optionalAnnotatedEntity = optionalAnnotatedEntity
@@ -24,7 +28,9 @@ extension JSONDeserializableProperty: JSONDeserializable {
 // MARK: - JSONDeserializableProperty.Entity AutoJSONDeserializable
 extension JSONDeserializableProperty.Entity: JSONDeserializable {
     internal init?(JSONObject: [String: Any]) {
-        guard let name = JSONObject["name"] as? String else { return nil }
+        guard
+        let name = JSONObject["name"] as? String
+        else { return nil }
         self.name = name
     }
 }
@@ -32,13 +38,19 @@ extension JSONDeserializableProperty.Entity: JSONDeserializable {
 // MARK: - MultiTypesProperties AutoJSONDeserializable
 extension MultiTypesProperties: JSONDeserializable {
     internal init?(JSONObject: [String: Any]) {
-        guard let string = JSONObject["string"] as? String else { return nil }
+        guard
+        let string = JSONObject["string"] as? String
+        else { return nil }
         self.string = string
-        guard let integer = JSONObject["integer"] as? Int else { return nil }
+        guard
+        let integer = JSONObject["integer"] as? Int
+        else { return nil }
         self.integer = integer
         let optionalInteger = JSONObject["optionalInteger"] as? Int
         self.optionalInteger = optionalInteger
-        guard let double = JSONObject["double"] as? Double else { return nil }
+        guard
+        let double = JSONObject["double"] as? Double
+        else { return nil }
         self.double = double
         let optionalDouble = JSONObject["optionalDouble"] as? Double
         self.optionalDouble = optionalDouble
@@ -56,7 +68,9 @@ extension OptionalProperty: JSONDeserializable {
 // MARK: - SinglePropertyNoAnnotation AutoJSONDeserializable
 extension SinglePropertyNoAnnotation: JSONDeserializable {
     internal init?(JSONObject: [String: Any]) {
-        guard let name = JSONObject["name"] as? String else { return nil }
+        guard
+        let name = JSONObject["name"] as? String
+        else { return nil }
         self.name = name
     }
 }
@@ -64,7 +78,9 @@ extension SinglePropertyNoAnnotation: JSONDeserializable {
 // MARK: - SinglePropertyWithKeyPathAnnotation AutoJSONDeserializable
 extension SinglePropertyWithKeyPathAnnotation: JSONDeserializable {
     internal init?(JSONObject: [String: Any]) {
-        guard let name = JSONObject["label"] as? String else { return nil }
+        guard
+        let name = JSONObject["label"] as? String
+        else { return nil }
         self.name = name
     }
 }
