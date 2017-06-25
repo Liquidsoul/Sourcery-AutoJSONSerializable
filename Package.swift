4e4d561bc1886b3ff31ca77ef54c34d2c35785db
Liquidsoul <liquidsoul@liquidsoul.fr>
2017-06-25 19:39:37 +0200

    Remove dependency to Sourcery in SPM

--- Package.swift
+++ Package.swift
@@ -3,8 +3,5 @@
 import PackageDescription
 
 let package = Package(
-  name: "AutoJSONSerialization",
-  dependencies: [
-    .Package(url: "https://github.com/krzysztofzablocki/Sourcery.git", majorVersion: 0, minor: 6)
-  ]
+  name: "AutoJSONSerialization"
 )
