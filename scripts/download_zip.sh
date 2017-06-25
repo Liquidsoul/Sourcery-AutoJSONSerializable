2a3ce9bb9dd8ea58a904c4fb6dcd50f358513e1a
Liquidsoul <liquidsoul@liquidsoul.fr>
2017-06-25 19:32:50 +0200

    Add download script for sourcery with Makefile rule

---
+++ scripts/download_zip.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# Download a zip file from a given URL and unzip it in the output folder.
+
+readonly PROGNAME=$(basename $0)
+
+is_file() {
+  local file=$1
+
+  [[ -f $file ]]
+}
+
+delete_file() {
+  local file_path=$1
+  is_file $file_path \
+    && rm $file_path
+}
+
+download() {
+  local install_dir=$1
+  local download_url=$2
+  curl -Lo $install_dir $download_url
+}
+
+main() {
+  if [[ $# -ne 2 ]]
+  then
+    echo "Invalid usage"
+    echo " $PROGNAME <OUTPUT_PATH> <URL>"
+    exit 1
+  fi
+
+  local output_dir=$1; shift
+  local download_url=$1; shift
+  local archive_path=/tmp/`basename $download_url`
+
+  delete_file $archive_path
+  download $archive_path $download_url
+  unzip $archive_path -d $output_dir
+}
+main $@
