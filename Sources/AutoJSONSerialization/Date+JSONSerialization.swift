76e647d2b4c3f8ec6fcfb295fabec9e7fb7da880
Liquidsoul <liquidsoul@liquidsoul.fr>
2017-06-29 23:17:03 +0200

    Remove pre-iOS10/macOS10.12 support in JSONDateFormatter

--- Sources/AutoJSONSerialization/Date+JSONSerialization.swift
+++ Sources/AutoJSONSerialization/Date+JSONSerialization.swift
@@ -18,37 +18,21 @@ extension Date: JSONDeserializable {
 
 enum JSONDateFormatter {
     static func date(from string: String) -> Date? {
-        if #available(iOS 10.0, macOS 10.12, *) {
-            // [HACK] workaround for unsupported milliseconds part in ISO8601DateFormatter.
-            // "1985-04-12T23:20:50Z" is supported where "1985-04-12T23:20:50.678Z" is not.
-            // So this removes the ".678" part.
-            let dateString = string.replacingOccurrences(of: "\\.\\d+", with: "", options: .regularExpression)
-            // [/HACK]
-            return isoDateFormatter.date(from: dateString)
-        } else {
-            return dateFormatter.date(from: string)
-        }
+        // [HACK] workaround for unsupported milliseconds part in ISO8601DateFormatter.
+        // "1985-04-12T23:20:50Z" is supported where "1985-04-12T23:20:50.678Z" is not.
+        // So this removes the ".678" part.
+        let dateString = string.replacingOccurrences(of: "\\.\\d+", with: "", options: .regularExpression)
+        // [/HACK]
+        return isoDateFormatter.date(from: dateString)
     }
 
     static func string(from date: Date) -> String {
-        if #available(iOS 10.0, macOS 10.12, *) {
-            return isoDateFormatter.string(from: date)
-        } else {
-            return dateFormatter.string(from: date)
-        }
+        return isoDateFormatter.string(from: date)
     }
 
-    @available(iOS 10.0, macOS 10.12, *)
     private static let isoDateFormatter: ISO8601DateFormatter = {
         let formatter = ISO8601DateFormatter()
         formatter.formatOptions = .withInternetDateTime
-        return formatter
-    }()
-
-    private static let dateFormatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.locale = Locale(identifier: "en_US_POSIX")
-        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
         return formatter
     }()
 }
